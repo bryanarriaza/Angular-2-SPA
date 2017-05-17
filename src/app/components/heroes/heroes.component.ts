ac77665e54fe16a15de51d81cfef9ee7f512c5db
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2017-05-16 18:30:25 -0600

    Agregado todo antes de mis cambios de la tarea 1 del curso

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  constructor( private _heroesService:HeroesService,
+               private _router:Router ){
+    //console.log("constructor");
+  }
+
+  ngOnInit(){
+    this.heroes = this._heroesService.getHeroes();
+    //console.log(this.heroes);
+  }
+
+  verHeroe( idx:number ){
+    this._router.navigate( ['/heroe', idx] );
+  }
+
+}
